8c336253acc3f5b6b3c6168969b104eaeb33fb7d
Umair Waheed <m.umairwaheedansari@gmail.com>
2026-05-10 16:04:51 +0500

    fix: prevent overscroll bounce on Android WebView via CSS and native WebView setting

--- android/app/src/main/java/com/cooklens/app/MainActivity.java
+++ android/app/src/main/java/com/cooklens/app/MainActivity.java
@@ -1,5 +1,22 @@
 package com.cooklens.app;
 
+import android.os.Bundle;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Disable overscroll glow/bounce effect on the WebView at the native level
+        WebView webView = getBridge().getWebView();
+        if (webView != null) {
+            webView.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        }
+    }
+}
